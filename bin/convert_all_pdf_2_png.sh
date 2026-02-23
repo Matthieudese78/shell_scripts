2977b0f57e9c3143c4b8f412a1f036255473c9b9
matthieu <66738778+Matthieudese78@users.noreply.github.com>
2026-02-23 07:54:28 +0100

    fix : convert_all_pdf_2_png.sh : syntax

--- bin/convert_all_pdf_2_png.sh
+++ bin/convert_all_pdf_2_png.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 density1='220x220'
-for i in ./**/*.pdf* ; do 
+for i in ./**/*.pdf ; do 
 # ps2pdf $i ${i%.ps}.pdf
 # gs -o ${i%.ps}.pdf -sDEVICE=pdfwrite -g8420x5950 -dPDFFitpage $i
 convert -density $density1 $i ${i%.pdf}.png 
